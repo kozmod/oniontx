a739a8087d5dd0ebb703085d74b1775aa875858c
kozmod <gizmod@bk.ru>
2023-08-06 10:13:40 +0300

    remove `O any` from default `ContextOperator`

--- operator.go
+++ operator.go
@@ -8,31 +8,31 @@ import (
 type CtxKeyType string
 
 // ContextOperator inject and extract TxCommitter rom context.Context.
-type ContextOperator[B TxBeginner[C, O], C TxCommitter, O any] struct {
+type ContextOperator[B any, C TxCommitter] struct {
 	beginner *B
 }
 
 // NewContextOperator creates new ContextOperator.
-func NewContextOperator[B TxBeginner[C, O], C TxCommitter, O any](b *B) *ContextOperator[B, C, O] {
-	return &ContextOperator[B, C, O]{
+func NewContextOperator[B any, C TxCommitter](b *B) *ContextOperator[B, C] {
+	return &ContextOperator[B, C]{
 		beginner: b,
 	}
 }
 
 // Inject injects TxCommitter.
-func (p *ContextOperator[B, C, O]) Inject(ctx context.Context, c C) context.Context {
+func (p *ContextOperator[B, C]) Inject(ctx context.Context, c C) context.Context {
 	key := p.Key()
 	return context.WithValue(ctx, key, c)
 }
 
 // Extract injects TxCommitter.
-func (p *ContextOperator[B, C, O]) Extract(ctx context.Context) (C, bool) {
+func (p *ContextOperator[B, C]) Extract(ctx context.Context) (C, bool) {
 	key := p.Key()
 	c, ok := ctx.Value(key).(C)
 	return c, ok
 }
 
 // Key get key (CtxKeyType) for injecting or extracting TxCommitter from context.Context.
-func (p *ContextOperator[B, C, O]) Key() CtxKeyType {
+func (p *ContextOperator[B, C]) Key() CtxKeyType {
 	return CtxKeyType(fmt.Sprintf("%p", p.beginner))
 }
