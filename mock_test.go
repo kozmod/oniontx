773ae73c96c4a497481ced0cb43abc93c97a0780
kozmod <kozmo23g@gmail.com>
2026-02-18 15:15:49 +0300

    [#183] first impl of `Sage`

---
+++ mock_test.go
@@ -0,0 +1,75 @@
+package oniontx
+
+import (
+	"context"
+	"testing"
+)
+
+// beginnerMock was added to avoid to use external dependencies for mocking (pointer receiver).
+type beginnerMock[T Tx] struct {
+	beginFn func(ctx context.Context) (T, error)
+}
+
+func (b *beginnerMock[T]) BeginTx(ctx context.Context) (T, error) {
+	return b.beginFn(ctx)
+}
+
+// committerMock was added to avoid to use external dependencies for mocking (pointer receiver).
+type committerMock struct {
+	commitFn   func(ctx context.Context) error
+	rollbackFn func(ctx context.Context) error
+}
+
+func (c *committerMock) Commit(ctx context.Context) error {
+	return c.commitFn(ctx)
+}
+
+func (c *committerMock) Rollback(ctx context.Context) error {
+	return c.rollbackFn(ctx)
+}
+
+// beginnerValueMock was added to avoid to use external dependencies for mocking (value receiver).
+type beginnerValueMock[T Tx] struct {
+	beginner *beginnerMock[T]
+}
+
+func (b beginnerValueMock[T]) BeginTx(ctx context.Context) (T, error) {
+	return b.beginner.beginFn(ctx)
+}
+
+// committerValueMock was added to avoid to use external dependencies for mocking (value receiver).
+type committerValueMock struct {
+	committer *committerMock
+}
+
+func (c committerValueMock) Commit(ctx context.Context) error {
+	return c.committer.commitFn(ctx)
+}
+
+func (c committerValueMock) Rollback(ctx context.Context) error {
+	return c.committer.commitFn(ctx)
+}
+
+// assertTrue was added to avoid to use external dependencies for mocking
+func assertTrue(t *testing.T, val bool) {
+	t.Helper()
+	if !val {
+		t.Fatal()
+	}
+}
+
+// assertFalse was added to avoid to use external dependencies for mocking
+func assertFalse(t *testing.T, val bool) {
+	t.Helper()
+	if val {
+		t.Fatal()
+	}
+}
+
+// assertNoError was added to avoid to use external dependencies for mocking
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("error arise: %v", err)
+	}
+}
---
+++ sage.go
@@ -0,0 +1,105 @@
+package oniontx
+
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrSagaAction       = fmt.Errorf("action failed")
+	ErrSagaCompensation = fmt.Errorf("compensation failed")
+)
+
+// Step of the [Sage].
+type Step struct {
+	// Name of the step.
+	Name string
+
+	// Transactor - a local transactor for a specific storage.
+	// It should implement an interface similar to [oniontx.Transactor].
+	Transactor interface {
+		WithinTx(ctx context.Context, fn func(ctx context.Context) error) (err error)
+	}
+
+	// Action is the main operation executed within a step's transaction.
+	Action func(ctx context.Context) error
+
+	// Compensation - a compensating action that undoes the Action (if possible).
+	// Called upon failure in subsequent steps
+	Compensation func(ctx context.Context) error
+}
+
+// Sage coordinates a distributed transaction using the `Saga` pattern.
+type Sage struct {
+	steps []Step
+}
+
+// NewSaga creates a new [Sage] instance.
+func NewSaga(steps []Step) *Sage {
+	return &Sage{
+		steps: steps,
+	}
+}
+
+// Execute runs all Saga steps.
+//
+// If any step fails, compensating actions are triggered for all successfully completed steps.
+func (s *Sage) Execute(ctx context.Context) error {
+	var completedSteps []Step
+
+	for i, step := range s.steps {
+		var err error
+		switch {
+		case step.Transactor == nil:
+			err = step.Action(ctx)
+			if err != nil {
+				return fmt.Errorf("step failed [%d:%s]: %w", i, step.Name, err)
+			}
+		default:
+			err = step.Transactor.WithinTx(ctx, func(txCtx context.Context) error {
+				err = step.Action(txCtx)
+				if err != nil {
+					return fmt.Errorf("step failed [%d:%s]: %w", i, step.Name, err)
+				}
+				return nil
+			})
+		}
+
+		if err != nil {
+			// Run compensation when error arise.
+			return s.compensate(ctx, completedSteps, err)
+		}
+
+		completedSteps = append(completedSteps, step)
+	}
+
+	return nil
+}
+
+// compensate triggers compensating actions for all steps in reverse order
+func (s *Sage) compensate(ctx context.Context, completedSteps []Step, originalErr error) error {
+	var compensationErrors []error
+
+	for i, step := range completedSteps {
+		if step.Compensation == nil {
+			continue
+		}
+
+		if err := step.Compensation(ctx); err != nil {
+			compensationErrors = append(
+				compensationErrors,
+				fmt.Errorf("compensation failed - step [%d:%s]: %w", i, step.Name, err),
+			)
+		}
+	}
+
+	if len(compensationErrors) > 0 {
+		return errors.Join(
+			fmt.Errorf("original error: %w,  compensation errors: %w", originalErr, errors.Join(compensationErrors...)),
+			ErrSagaCompensation,
+		)
+	}
+
+	return errors.Join(originalErr, ErrSagaAction)
+}
