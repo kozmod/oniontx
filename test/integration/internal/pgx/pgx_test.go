90f8a029a6cbec213420c7802863736d2b4fcdd1
kozmod <gizmod@bk.ru>
2026-08-25 22:40:37 +0300

    [#0] `test/integration/internal/sqlx/pgx_test`: assert -> require

--- test/integration/internal/pgx/pgx_test.go
+++ test/integration/internal/pgx/pgx_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/kozmod/oniontx/test/integration/internal/entity"
 )
@@ -20,12 +20,12 @@ func Test_UseCase_CreateTextRecords(t *testing.T) {
 	)
 	defer func() {
 		err := db.Close(globalCtx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	}()
 
 	cleanupFn := func() {
 		err := ClearDB(globalCtx, db)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	}
 
 	cleanupFn()
@@ -42,14 +42,14 @@ func Test_UseCase_CreateTextRecords(t *testing.T) {
 		)
 
 		err := useCase.CreateTextRecords(ctx, textRecord)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		{
 			records, err := GetTextRecords(globalCtx, db)
-			assert.NoError(t, err)
-			assert.Len(t, records, 2)
+			require.NoError(t, err)
+			require.Len(t, records, 2)
 			for _, record := range records {
-				assert.Equal(t, textRecord, record)
+				require.Equal(t, textRecord, record)
 			}
 		}
 	})
@@ -65,13 +65,13 @@ func Test_UseCase_CreateTextRecords(t *testing.T) {
 		)
 
 		err := useCase.CreateTextRecords(ctx, textRecord)
-		assert.Error(t, err)
-		assert.ErrorIs(t, err, entity.ErrExpected)
+		require.Error(t, err)
+		require.ErrorIs(t, err, entity.ErrExpected)
 
 		{
 			records, err := GetTextRecords(globalCtx, db)
-			assert.NoError(t, err)
-			assert.Len(t, records, 0)
+			require.NoError(t, err)
+			require.Len(t, records, 0)
 
 		}
 	})
@@ -88,13 +88,13 @@ func Test_UseCase_CreateTextRecords(t *testing.T) {
 
 		cancel()
 		err := useCase.CreateTextRecords(ctx, textRecord)
-		assert.Error(t, err)
-		assert.ErrorIs(t, err, context.Canceled)
+		require.Error(t, err)
+		require.ErrorIs(t, err, context.Canceled)
 
 		{
 			records, err := GetTextRecords(globalCtx, db)
-			assert.NoError(t, err)
-			assert.Len(t, records, 0)
+			require.NoError(t, err)
+			require.Len(t, records, 0)
 		}
 	})
 }
@@ -106,12 +106,12 @@ func Test_UseCases(t *testing.T) {
 	)
 	defer func() {
 		err := db.Close(globalCtx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	}()
 
 	cleanupFn := func() {
 		err := ClearDB(globalCtx, db)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	}
 
 	cleanupFn()
@@ -133,14 +133,14 @@ func Test_UseCases(t *testing.T) {
 			)
 
 			err := useCases.CreateTextRecords(ctx, textRecord)
-			assert.NoError(t, err)
+			require.NoError(t, err)
 
 			{
 				records, err := GetTextRecords(globalCtx, db)
-				assert.NoError(t, err)
-				assert.Len(t, records, 4)
+				require.NoError(t, err)
+				require.Len(t, records, 4)
 				for _, record := range records {
-					assert.Equal(t, textRecord, record)
+					require.Equal(t, textRecord, record)
 				}
 			}
 		})
@@ -160,13 +160,13 @@ func Test_UseCases(t *testing.T) {
 			)
 
 			err := useCases.CreateTextRecords(ctx, textRecord)
-			assert.Error(t, err)
-			assert.ErrorIs(t, err, entity.ErrExpected)
+			require.Error(t, err)
+			require.ErrorIs(t, err, entity.ErrExpected)
 
 			{
 				records, err := GetTextRecords(globalCtx, db)
-				assert.NoError(t, err)
-				assert.Len(t, records, 0)
+				require.NoError(t, err)
+				require.Len(t, records, 0)
 			}
 		})
 	})
