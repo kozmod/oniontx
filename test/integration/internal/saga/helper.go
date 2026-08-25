f94a7bc37c05f0908fbceb3058c7ebee4ac68921
kozmod <gizmod@bk.ru>
2026-08-25 22:51:51 +0300

    [#0] `test/integration/internal/sqlx/saga/`: add `strings.Builder` to `printResult`

--- test/integration/internal/saga/helper.go
+++ test/integration/internal/saga/helper.go
@@ -2,6 +2,7 @@ package saga
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/kozmod/oniontx/saga"
@@ -10,41 +11,47 @@ import (
 func printResult(t *testing.T, res saga.Result, err error) {
 	t.Helper()
 
-	t.Logf("\n")
-	fmt.Printf("[result]:\n")
-	fmt.Printf("%v\n", res)
-	fmt.Printf("[execution error]:\n%v\n", err)
-	fmt.Printf("\n[steps]:")
+	var sb strings.Builder
+	writef := func(format string, a ...any) {
+		_, _ = sb.WriteString(fmt.Sprintf(format, a...))
+	}
+
+	writef("[result]:\n")
+	writef("%v\n", res)
+	writef("[execution error]:\n%v\n", err)
+	writef("\n[steps]:")
 
 	for _, step := range res.Steps {
-		fmt.Printf("\nstep [%d#%s]:\n", step.StepPosition, step.StepName)
+		writef("\nstep [%d#%s]:\n", step.StepPosition, step.StepName)
 
 		switch {
 		case len(step.Action.Errors) > 0:
-			fmt.Printf("  action errors (%d):\n", len(step.Action.Errors))
+			writef("  action errors (%d):\n", len(step.Action.Errors))
 			for i, e := range step.Action.Errors {
-				fmt.Printf("    %d: %v\n", i, e)
+				writef("    %d: %v\n", i, e)
 			}
 		default:
-			fmt.Printf("  action errors: none\n")
+			writef("  action errors: none\n")
 		}
 
 		switch {
 		case len(step.Compensation.Errors) > 0:
-			fmt.Printf("  compensation errors (%d):\n", len(step.Compensation.Errors))
+			writef("  compensation errors (%d):\n", len(step.Compensation.Errors))
 			for i, e := range step.Compensation.Errors {
-				fmt.Printf("    %d: %v\n", i, e)
+				writef("    %d: %v\n", i, e)
 			}
 		default:
-			fmt.Printf("  compensation errors: none\n")
+			writef("  compensation errors: none\n")
 		}
 
-		fmt.Printf("  -----\n")
-		fmt.Printf("  action status: %v\n", step.Action.Status)
-		fmt.Printf("  compensation status: %v\n", step.Compensation.Status)
-		fmt.Printf("  compensation on action failure: %v\n", step.CompensationOnActionFailure)
-		fmt.Printf("  action calls: %d\n", step.Action.Calls)
-		fmt.Printf("  compensation calls: %d\n", step.Compensation.Calls)
-		fmt.Printf("-----\n")
+		writef("  -----\n")
+		writef("  action status: %v\n", step.Action.Status)
+		writef("  compensation status: %v\n", step.Compensation.Status)
+		writef("  compensation on action failure: %v\n", step.CompensationOnActionFailure)
+		writef("  action calls: %d\n", step.Action.Calls)
+		writef("  compensation calls: %d\n", step.Compensation.Calls)
+		writef("-----\n")
+
+		t.Logf("\n%s", sb.String())
 	}
 }
